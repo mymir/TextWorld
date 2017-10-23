bd4eb1a4cc9db6e5e5f48fc35d2aca02ba7c7f5b
Rafael Zingle <rmzingle@ncsu.edu>
2017-10-23 11:34:23 -0400

    "Added classes to the Inventory package"

---
+++ MapMaker/src/inventory/Item.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package inventory;
+
+/**
+ * @author rafaelzingle
+ *
+ */
+public abstract class Item {
+
+}
---
+++ MapMaker/src/inventory/QuestItem.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package inventory;
+
+/**
+ * @author rafaelzingle
+ *
+ */
+public class QuestItem extends Item {
+
+	/**
+	 * 
+	 */
+	public QuestItem() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
